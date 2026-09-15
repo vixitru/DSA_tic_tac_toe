42a843b6cf3235300854483927830665f0d5473e
Henbrickson0 <128AHalaburda@frhsd.com>
2026-09-15 13:46:35 -0400

    Added more methods to the Board class

--- src/Board.java
+++ src/Board.java
@@ -2,19 +2,13 @@ import java.util.Scanner;
 
 public class Board {
     private char[][] board;
-    int rows = 0;
-    int cols = 0;
-
-
 
     Scanner playerInput = new Scanner(System.in);
     boolean player1Turn = true;
 
-    public Board(int rows, int cols)
+    public Board()
     {
-        char[][] board = new char[rows][cols];
-        this.rows = rows;
-        this.cols = cols;
+        char[][] board = new char[3][3];
     }
 
     public char[][] getBoard()
@@ -22,9 +16,28 @@ public class Board {
         return board;
     }
 
-    public void placeTile()
+    public void setPlayer1Turn(boolean set)
     {
+        player1Turn = set;
+    }
 
+    public boolean getPlayer1Turn()
+    {
+        return player1Turn;
+    }
+
+    public void placeTile(int row, int col, char [][] board, boolean player1Turn)
+    {
+        if (player1Turn)
+        {
+            board[row][col] = 'X';
+            setPlayer1Turn(false);
+        }
+        else
+        {
+            board[row][col] = 'O';
+            setPlayer1Turn(true);
+        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    Board board = new Board();
+    char gameBoard[][] = board.getBoard();
+    boolean player1Turn = board.getPlayer1Turn();
+
+    boolean isGameOver = false;
+
+
 }
