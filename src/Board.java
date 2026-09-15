540390983b8e51dac7426292180d7914c353be9f
Henbrickson0 <128AHalaburda@frhsd.com>
2026-09-15 13:32:33 -0400

    Added constructor to Board class and two methods, one of which is still being worked on.

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+
 public class Board {
     private char[][] board;
+    int rows = 0;
+    int cols = 0;
+
+
+
+    Scanner playerInput = new Scanner(System.in);
+    boolean player1Turn = true;
+
+    public Board(int rows, int cols)
+    {
+        char[][] board = new char[rows][cols];
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    public char[][] getBoard()
+    {
+        return board;
+    }
+
+    public void placeTile()
+    {
+
+    }
+
+
+
+
 }
