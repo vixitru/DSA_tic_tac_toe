a8318d6a1ecbdaa3fc33356cd8b5920d4fa95aa0
vixitru <128ahanrahan@frhsd.com>
2026-09-15 13:52:22 -0400

    Creates Check Method

--- src/Board.java
+++ src/Board.java
@@ -38,6 +38,13 @@ public class Board {
             board[row][col] = 'O';
             setPlayer1Turn(true);
         }
+        public String Check(){
+           for(int i=0;i<3;i++){
+               for(int j=0;j<3;j++){
+
+               }
+           }
+    }
     }
 
 
